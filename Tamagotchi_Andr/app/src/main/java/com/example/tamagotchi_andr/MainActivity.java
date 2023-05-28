23bc0f52b8c680b190c33d97ac587587b1e6491c
quentinmel <112957970+quentinmel@users.noreply.github.com>
2023-05-28 15:07:12 +0200

    add death condition

--- Tamagotchi_Andr/app/src/main/java/com/example/tamagotchi_andr/MainActivity.java
+++ Tamagotchi_Andr/app/src/main/java/com/example/tamagotchi_andr/MainActivity.java
@@ -4,11 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
@@ -44,6 +46,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        checkHappiness();
+        checkSaturation();
 
         ImageView gifImageView = findViewById(R.id.gif_allay);
         Glide.with(this)
@@ -87,6 +92,56 @@ public class MainActivity extends AppCompatActivity {
                 showInventory();
             }
         });
+    }
+
+    private void checkHappiness() {
+        if (hapiness <= 0) {
+            // Death message
+            Toast toast = Toast.makeText(MainActivity.this, "Oh no! Your Allay away due to a great depression. It's a sadness moment. 😢 \n You start over from the beginning.", Toast.LENGTH_LONG);
+            toast.setGravity(Gravity.CENTER, 0, 0);
+            toast.setDuration(Toast.LENGTH_LONG * 4);
+            toast.show();
+
+            resetValues();
+            restartActivity();
+        }
+    }
+
+    private void checkSaturation() {
+        if (saturation <= 0) {
+            // Death message
+            Toast toast = Toast.makeText(MainActivity.this, "Oh no! Your Allay away due to extreme hunger. It's a sadness moment. 😢 \n You start over from the beginning.", Toast.LENGTH_LONG);
+            toast.setGravity(Gravity.CENTER, 0, 0);
+            toast.setDuration(Toast.LENGTH_LONG * 4);
+            toast.show();
+
+            resetValues();
+            restartActivity();
+        }
+    }
+
+    private void resetValues() {
+        money = 20;
+        hapiness = 100;
+        saturation = 40;
+
+        frenchBreadState = 0;
+        freshBeetrootState = 0;
+        crunchyCarrotState = 0;
+        bakedPotatoState = 0;
+        juicyAppleState = 0;
+
+        stoneHoeState = 0;
+        ironShovelState = 0;
+        goldenAxeState = 0;
+        diamondPickaxeState = 0;
+        netheriteSwordState = 0;
+    }
+
+    private void restartActivity() {
+        Intent intent = getIntent();
+        finish();
+        startActivity(intent);
     }
 
     private void showGames() {
